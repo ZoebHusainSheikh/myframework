12e0b418696a5f57c56d8574eecf5e409632db12
Zoeb Sheikh <zsheikh@isystango.com>
2017-01-05 17:27:50 +0530

    commit 2

---
+++ MyFramework/BViewController.h
@@ -0,0 +1,13 @@
+//
+//  BViewController.h
+//  MyFramework
+//
+//  Created by Systango on 05/01/17.
+//  Copyright © 2017 Systango. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BViewController : UIViewController
+
+@end
---
+++ MyFramework/BViewController.m
@@ -0,0 +1,38 @@
+//
+//  BViewController.m
+//  MyFramework
+//
+//  Created by Systango on 05/01/17.
+//  Copyright © 2017 Systango. All rights reserved.
+//
+
+#import "BViewController.h"
+
+@interface BViewController ()
+
+@end
+
+@implementation BViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+    NSLog(@"B");
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
